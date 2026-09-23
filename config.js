71d615be53f14c0795a829e77e1821a3b0194137
japnishpaints <japnishpaints@gmail.com>
2026-09-23 14:48:26 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 window.JP_CONFIG = {
   API_BASE_URL: "https://japnishpaints.store/api",
   DATA_API_BASE_URL: "https://japnishpaints.store/mobile/mobile-api.php",
-  APP_VERSION: "74.0.0",
+  APP_VERSION: "75.0.0",
   UPDATE_VERSION_URL: "https://japnishpaints.store/mobile/app-version.php",
   APK_DOWNLOAD_URL: "https://japnishpaints.store/mobile/app/JapnishPaints.apk",
   ENDPOINTS: {
